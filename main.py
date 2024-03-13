a60fcd66e9914cc73f6fc8adfcd5114fb6ca552f
cynic <kurisufag1@gmail.com>
2024-03-13 09:21:22 -0400

    lame

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ import os
 
 # e.g.
 channels = [
-    ("~/archival/bea", "@beatanichisato", "bea"),
-    ("~/archival/schlatt", "@jschlattvault", "jschlattvault"),
-    ("~/archival/hehe", "@Sillypoo", "sillypoo"),
-    ("~/archival/hehe", "@heheSilly", "hehesilly"),
-    ("~/archival/hime/PL", "@@XCHANMUSIC", "xchan"),
-    ("~/archival/pengiscool", "@flypenguins1119", "peng")
+    ("/home/rishi/archival/bea", "@beatanichisato", "bea"),
+    ("/home/rishi/archival/schlatt", "@jschlattvault", "jschlattvault"),
+    ("/home/rishi/archival/hehe", "@Sillypoo", "sillypoo"),
+    ("/home/rishi/archival/hehe", "@heheSilly", "hehesilly"),
+    ("/home/rishi/archival/hime/PL", "@@XCHANMUSIC", "xchan"),
+    ("/home/rishi/archival/pengiscool", "@flypenguins1119", "peng")
 ]
 template = "yt-dlp -f --embed-thumbnail --embed-metadata --download-archive $PENISARCHIVE$.txt https://www.youtube.com/$PENISCHANNEL$ -o '%(channel)s/%(title)s.%(ext)s'"
 
 for chan in channels:
     os.chdir(chan[0])
     os.system(template.replace("$PENISARCHIVE$", chan[2]).replace("$PENISCHANNEL$", chan[1]))
-    
