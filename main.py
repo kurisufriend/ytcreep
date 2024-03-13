667d141cd642aa20f7ef9c4ee16d9512958f6aa5
cynic <admin@cynic.moe>
2024-03-13 14:08:01 +0000

    default quality

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import os
 channels = [
     ("/home/rishi/archival/bea", "@beatanichisato", "bea"),
     ("/home/rishi/archival/schlatt", "@jschlattvault", "jschlattvault"),
-    ("/home/rishi/archival/hehe", "@Sillypoo", "sillypoo"),
-    ("/home/rishi/archival/hehe", "@heheSilly", "hehesilly"),
+    ("/home/rishi/archival/silly", "@Sillypoo", "sillypoo"),
+    ("/home/rishi/archival/silly", "@heheSilly", "hehesilly"),
     ("/home/rishi/archival/hime/PL", "@@XCHANMUSIC", "xchan"),
     ("/home/rishi/archival/pengiscool", "@flypenguins1119", "peng")
 ]
-template = "yt-dlp -f --embed-thumbnail --embed-metadata --download-archive $PENISARCHIVE$.txt https://www.youtube.com/$PENISCHANNEL$ -o '%(channel)s/%(title)s.%(ext)s'"
+template = "yt-dlp --embed-thumbnail --embed-metadata --download-archive $PENISARCHIVE$.txt https://www.youtube.com/$PENISCHANNEL$ -o '%(channel)s/%(title)s.%(ext)s'"
 
 for chan in channels:
     os.chdir(chan[0])
