c5480d75f62625597bb025a46a1692755278fdeb
cynic <admin@cynic.moe>
2024-03-13 14:09:10 +0000

    fix xchan path

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ channels = [
     ("/home/rishi/archival/schlatt", "@jschlattvault", "jschlattvault"),
     ("/home/rishi/archival/silly", "@Sillypoo", "sillypoo"),
     ("/home/rishi/archival/silly", "@heheSilly", "hehesilly"),
-    ("/home/rishi/archival/hime/PL", "@@XCHANMUSIC", "xchan"),
+    ("/home/rishi/archival/hime/PL", "@XCHANMUSIC", "xchan"),
     ("/home/rishi/archival/pengiscool", "@flypenguins1119", "peng")
 ]
 template = "yt-dlp --embed-thumbnail --embed-metadata --download-archive $PENISARCHIVE$.txt https://www.youtube.com/$PENISCHANNEL$ -o '%(channel)s/%(title)s.%(ext)s'"
